e41e61c0276bacf5887bf1f02c992b3ede096727
monowing <monowing1203@gmail.com>
2026-07-28 18:32:40 +0800

    修改了一些格式

--- lesson02_disk_basics.py
+++ lesson02_disk_basics.py
@@ -11,31 +11,22 @@ def main():
     print(f"磁盘分区数量:{len(partitions)}")
     
     if partitions:
-        first_partitions = partitions[0]
+        first_partition = partitions[0]
         
         print(f"\n第一个磁盘分区的信息:")
-        
-        print(f"设备名称:{first_partitions.device}")
-        
-        print(f"挂载点:{first_partitions.mountpoint}")
-        
-        print(f"文件系统:{first_partitions.fstype}")
-        
-        print(f"挂载选项:{first_partitions.opts}")
+        print(f"设备名称:{first_partition.device}")
+        print(f"挂载点:{first_partition.mountpoint}")
+        print(f"文件系统:{first_partition.fstype}")
+        print(f"挂载选项:{first_partition.opts}")
         
     # 获取根目录“/”所在磁盘的容量信息
     root_usage = psutil.disk_usage("/")
     
     print("\n根目录所在磁盘的使用情况:")
-    
-    print(f"总容量:{bytes_to_gb(root_usage.total)}")
-    
-    print(f"已使用:{bytes_to_gb(root_usage.used)}")
-    
-    print(f"剩余容量:{bytes_to_gb(root_usage.free)}")
-    
-    print(f"使用率:{root_usage.percent}")
-    
+    print(f"总容量:{bytes_to_gb(root_usage.total)} GB")
+    print(f"已使用:{bytes_to_gb(root_usage.used)} GB")
+    print(f"剩余容量:{bytes_to_gb(root_usage.free)} GB")
+    print(f"使用率:{root_usage.percent} %")
     print("\n所有磁盘分区：")
 
     for partition in partitions:
